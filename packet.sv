6e62c814574942a762b151685bf6bc5d6829edb2
sureshwarkesiraju <suresh061103war@gmail.com>
2025-07-15 08:24:05 +0000

    added Driver , generator class , mailbox used

--- packet.sv
+++ packet.sv
@@ -1,36 +1,31 @@
-class packet;
+//sa ,da,len,crc,payload
+//Section P.1 Define enum (control knob) to identify transaction type
+typedef enum {
+  IDLE,
+  RESET,
+  STIMULUS
+} pkt_type_t;
 
+class packet;
   rand bit [7:0] sa;
   rand bit [7:0] da;
   bit [31:0] len;
   bit [31:0] crc;
   rand bit [7:0] payload[];
+
   bit [7:0] inp_stream[$];
   bit [7:0] outp_stream[$];
 
+  //Section P.2: Define kind variable of type enum pkt_type_t.
+  pkt_type_t kind;
 
-
-  //Print function
-  function void print();
-    $write("[TB Packet] Sa=%0d Da=%0d Len=%0d crc=%0d", sa, da, len, crc);
-    foreach (payload[k]) $write("[TB Packet] Payload[%0d]=%0d\n", k, payload[k]);
-  endfunction
-
-
-  //Pack function
-  function void pack(ref bit [7:0] q_inp[$]);
-    q_inp = {<<8{this.payload, this.crc, this.len, this.da, this.sa}};
-  endfunction
-
-  // Unpack function
-  function void unpack(ref bit [7:0] q_inp[$]);
-    {<<8{this.payload, this.crc, this.len, this.da, this.sa}} = q_inp;
-  endfunction
+  //Section P.3: Define reset_cycles variable.
+  bit [7:0] reset_cycles;
 
   constraint valid {
     sa inside {[1 : 8]};
     da inside {[1 : 8]};
-    payload.size() inside {[2 : 1900]};
+    payload.size() inside {[10 : 100]};
     foreach (payload[i]) payload[i] inside {[0 : 255]};
   }
 
@@ -40,9 +35,17 @@ class packet;
     this.pack(inp_stream);
   endfunction
 
+  function void pack(ref bit [7:0] q_inp[$]);
+    q_inp = {<<8{this.payload, this.crc, this.len, this.da, this.sa}};
+  endfunction
+
+  function void unpack(ref bit [7:0] q_inp[$]);
+    {<<8{this.payload, this.crc, this.len, this.da, this.sa}} = q_inp;
+  endfunction
+
   function void copy(packet rhs);
     if (rhs == null) begin
-      $display("[ERROR] Empty packet recived");
+      $display("[Error] NULL handle passed to copy method");
       $finish;
     end
     this.sa = rhs.sa;
@@ -50,19 +53,25 @@ class packet;
     this.len = rhs.len;
     this.crc = rhs.crc;
     this.payload = rhs.payload;
+    //Section P.4: Copy the packed strema , this we use it in driver to drive transaction
     this.inp_stream = rhs.inp_stream;
   endfunction
-
 
   function bit compare(input packet dut_pkt);
     bit status;
     status = 1;
-
     foreach (dut_pkt.inp_stream[i]) begin
-      $display(this.inp_stream[i]);
-      $display(dut_pkt.outp_stream[i]);
       status = status && (this.inp_stream[i] == dut_pkt.outp_stream[i]);
     end
     return status;
   endfunction
+
+  function void print();
+    $write("[Packet Print] Sa=%0d Da=%0d Len=%0d Crc=%0d", sa, da, len, crc);
+    $write(" Payload:");
+    foreach (payload[k]) $write(" %0h", payload[k]);
+
+    $display("\n");
+  endfunction
+
 endclass
--- testbench_prgrm.sv
+++ testbench_prgrm.sv
@@ -5,157 +5,40 @@ program testbench (
 
   //Section4: TB Variables declarations. 
   //Variables required for various testbench related activities . 
-  //ex: stimulus generation,packing ....
+  //ex: class handles
 
-  //Section 4.1 : Include packet.sv 
+  //Section 4.1 : Include packet,generator and driver classes
+
   `include "packet.sv"
+  `include "Generator.sv"
+  `include "Driver.sv"
+  //Section 4.2 : Define generator,driver and mailbox class handles
+  generator gen;
+  driver drv;
+  mailbox #(packet) mbx;
 
-  //Section 4.2 : Define packet class handles
-  packet stimulus_pkt;
-  packet dut_pkt;
-
-  //Section 4.3 : Define local outp_stream to capture packet from dut
-  bit [7:0] outp_stream[$];
-
-  //Section 4.4: Store input stimulus packets into q_inp : Reference packet
-  packet q_inp[$];  //queue of objects
-
-  //Section 4.5 : Store dut output packet into q_outp : Actual packet
-  packet q_outp[$];  //queue of objects
-
-  //Section 4.6 : Stimulus packet count and packet id
-  bit [15:0] pkt_count, pkt_id;
+  //Section 4.3 : Stimulus packet count 
+  bit [15:0] pkt_count;
 
   //Section 6: Verification Flow
   initial begin
     //Section 6.1 : How many number of packets to generate
-
     pkt_count = 10;
-    //Section 6.2 : Call apply_reset() method.
-    apply_reset();
-    repeat (pkt_count) begin
-      wait (vif.cb.busy == 0);
-      pkt_id++;
-      //Section 6.3 : Construct stimulus packet Object
-      stimulus_pkt = new();
+    //Section 6.2 : Construct objects for mailbox,generator and driver.
+    //Also connect vif(interface) port of program block to driver virtual interface.
+    mbx = new(1);
+    gen = new(mbx, pkt_count);
+    drv = new(mbx, vif);
 
-      //Section 6.4 : Generate random stimulus.
-      void'(stimulus_pkt.randomize());
-      //$display(stimulus_pkt.inp_stream);
-      //Section 6.5 : Store Reference/Golden packet into q_inp.
-      q_inp.push_back(stimulus_pkt);
-
-      //Section 6.6 : Call drive() method.
-      drive(stimulus_pkt, pkt_id);
-      repeat (5) @(vif.cb);
-    end
+    //Section 6.3 : Start generator and driver
+    fork
+      gen.run();
+      drv.run();
+    join_any
 
     //Wait for dut to process the packet and to drive on output
     wait (vif.cb.busy == 0);  //drain time
-    repeat (10) @(vif.cb);  //drain time
-    result();
+    repeat (25) @(vif.cb);  //drain time
     $finish;
   end
-
-  //Section 5: Methods (functions/tasks) definitions related to Verification Environment 
-
-  task apply_reset();
-    $display("[TB Reset] Applied reset to DUT at time=%0t", $time);
-    vif.reset <= 1;
-    repeat (2) @(vif.cb);
-    vif.reset <= 0;
-    $display("[TB Reset] Reset Completed at time=%0t \n", $time);
-  endtask
-
-  //Section 5.1 : Define drive() method
-
-  task automatic drive(ref packet pkt, input int ptk_id);
-    wait (vif.cb.busy == 0);
-    @(vif.cb);
-    $display("[TB Drive] Driving of packet %0d (size=%0d) started at time=%0t", pkt_id, pkt.len,
-             $time);
-    vif.cb.inp_valid <= 1;
-    foreach (pkt.inp_stream[i]) begin
-      vif.cb.dut_inp <= pkt.inp_stream[i];
-      @(vif.cb);
-    end
-    $display("[TB Drive] Driving of packet %0d (size=%0d) ended at time=%0t", pkt_id, pkt.len,
-             $time);
-    //    @(vif.cb);
-    vif.cb.inp_valid <= 0;
-    vif.cb.dut_inp   <= 'z;
-    repeat (5) @(vif.cb);
-  endtask
-
-  //Section 5.2 : Define result() method
-  function void result();
-    bit [31:0] matched, mis_matched;
-
-    foreach (q_inp[i]) begin
-      //Fill the code here from LAB document
-      if (q_inp[i].compare(q_outp[i])) matched++;
-      else begin
-        mis_matched++;
-        $display("[TB ERROR] packet %0d MisMatched", i);
-        q_inp[i].print();
-        q_outp[i].print();
-        $display("***************************************\n");
-      end
-    end  //end_of_foreach
-
-    if (mis_matched == 0 && matched == pkt_count) begin
-      $display("\n[INFO] *************************************");
-      $display("[INFO] ************Test PASSED *************");
-      $display("[INFO] *Tot_pkts=%0d Matched=%0d mis_matched=%0d", pkt_count, matched, mis_matched);
-      $display("[INFO] *************************************\n");
-    end else begin
-      $display("\n[INFO] *************************************");
-      $display("[INFO] ************Test FAILED *************");
-      $display("[INFO] *Tot_pkts=%0d Matched=%0d mis_matched=%0d", pkt_count, matched, mis_matched);
-      $display("[INFO] *************************************\n");
-    end
-  endfunction
-
-  //Section 8: Collecting DUT output
-  initial begin
-    bit [15:0] cnt;
-    forever begin
-      @(posedge vif.cb.outp_valid);
-      while (1) begin
-        //Section 8.1 : Capture complete packet from DUT
-        outp_stream.push_back(vif.cb.dut_outp);
-        //$display(outp_stream);
-        //Section 8.2 : Collect untill outp_valid becomes 0.
-        if (vif.cb.outp_valid == 0) begin
-
-          //Section 8.3 : Increment the cnt to track how many output packets collected
-          cnt++;
-
-          //Section 8.4 : Construct dut_pkt object to store the collected output
-          dut_pkt = new;
-
-          //Section 8.5 : Unpack collected outp_stream into dut_pkt fields
-          dut_pkt.unpack(outp_stream);
-
-          //Section 8.6 : Copy local outp_stream to outp_stream in dut_pkt
-          dut_pkt.outp_stream = outp_stream;
-          //dut_pkt.print();
-          //Section 8.7 : Store the actual packet from DUT for sel-checking
-          q_outp.push_back(dut_pkt);
-
-          $display("[TB Output Monitor] Packet %0d collected size=%0d time=%0t \n", cnt,
-                   outp_stream.size(), $time);
-          //Section 8.8 : Delete local outp_stream queue
-          outp_stream.delete();
-
-          //Section 8.9 : Break out of while loop as collection of packet completed.
-          break;
-        end
-        //Section 8.10 : Wait for posedge of clk to collect all the dut output
-        @(vif.cb);
-
-      end  //end_of_while
-    end  //end_of_forever
-  end  //end_of_initial
-
 endprogram
